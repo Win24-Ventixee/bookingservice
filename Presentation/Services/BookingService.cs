7db2d6057ceba70b309ed188eaf05ebe09bd422a
Milan Dzodic <milan.dzodic@hotmail.com>
2025-06-04 13:22:57 +0200

    Add project files.

---
+++ Presentation/Services/BookingService.cs
@@ -0,0 +1,38 @@
+﻿using Presentation.Data.Entities;
+using Presentation.Data.Repositories;
+using Presentation.Models;
+
+namespace Presentation.Services;
+
+public class BookingService(IBookingRepository bookingRepository) : IBookingService
+{
+  private readonly IBookingRepository _bookingRepository = bookingRepository;
+
+  public async Task<BookingResult> CreateBookingAsync(CreateBookingRequest request)
+  {
+    var bookingEntity = new BookingEntity
+    {
+      EventId = request.EventId,
+      BookingDate = DateTime.Now,
+      TicketQuantity = request.TicketQuantity,
+      Owner = new BookingOwnerEntity
+      {
+        Firstname = request.FirstName,
+        Lastname = request.LastName,
+        Email = request.Email,
+        Address = new BookingAddressEntity
+        {
+          StreetName = request.StreeetName,
+          PostalCode = request.PostalCode,
+          City = request.City,
+        }
+      }
+    };
+
+    var result = await _bookingRepository.AddAsync(bookingEntity);
+
+    return result.Success
+      ? new BookingResult { Success = true }
+      : new BookingResult { Success = false, Error = result.Error };
+  }
+}
